cb32441c9167bbbeff843849c63e9f5549f507b2
ngiad <follownghia001@gmail.com>
2023-11-22 12:10:36 +0700

    done3

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,33 +1,44 @@
-import React, { useEffect } from 'react'
-import { Link, Outlet, useNavigate } from 'react-router-dom'
-import "../styles/layout.css"
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import React, { useEffect } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import "../styles/layout.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
-
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div>
-        <nav className='header'>
-          <div className='left'>
-            <Link to="/">Logo</Link>
-          </div>
-          <div className='right'>
-            <ul>
-              <li><Link to={"/login"}>Đăng nhập</Link></li>
-              <li><Link to={"/register"}>Đăng kí</Link></li>
-            </ul>
-          </div>
-        </nav>
-        <div style={{padding: "40px 0"}}>
-            <Outlet />
+      <nav className="header">
+        <div className="left">
+          <Link to="/">Logo</Link>
         </div>
-        <div className='footer'>
-            <p>@by : Tran Dai Nghia</p>
+        <div className="right">
+          <ul>
+            {localStorage.getItem("auth") ? (
+              <li>
+                <Link to={"/1"}>Tasks</Link>
+              </li>
+            ) : (
+              <>
+                <li>
+                  <Link to={"/login"}>Đăng nhập</Link>
+                </li>
+                <li>
+                  <Link to={"/register"}>Đăng kí</Link>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-        <ToastContainer />
+      </nav>
+      <div style={{ padding: "40px 0" }}>
+        <Outlet />
+      </div>
+      <div className="footer">
+        <p>@by : Tran Dai Nghia</p>
+      </div>
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/components/Private.jsx
+++ src/components/Private.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { CiLogout } from "react-icons/ci";
 
 const Private = () => {
   const navigate = useNavigate();
@@ -9,8 +10,14 @@ const Private = () => {
   useEffect(() => {
     if (!JSON.parse(localStorage.getItem("auth"))?.jwt) navigate("/login");
   }, [window.location.pathname]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("auth")
+    navigate("/")
+  }
   return (
     <div>
+      <button onClick={handleLogout} style={{position : "fixed", left : "0px", top : "140px",cursor : "pointer"}}><CiLogout style={{fontSize : "30px"}} /></button>
       <Outlet />
       <div>
         <p style={{textAlign : "center"}}>@ by : tran dai nghia</p>
